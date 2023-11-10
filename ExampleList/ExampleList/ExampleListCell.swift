1813e43681469bbaa75b5c7fb2f62abbf5e89e94
NohJooYoung <nohjoo97@naver.com>
2023-11-10 15:20:05 +0900

    [add] 실습 예제 리스트 임시 구현

---
+++ ExampleList/ExampleList/ExampleListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleListCell.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/10.
+//
+
+import UIKit
+
+class ExampleListCell: UITableViewCell {
+
+    static let identifier = "ExampleListCell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
--- ExampleList/ExampleList/ExampleListVC.swift
+++ ExampleList/ExampleList/ExampleListVC.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class ExampleListVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.title = "Example List"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ExampleListVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: ExampleListCell = tableView.dequeueReusableCell(withIdentifier: ExampleListCell.identifier, for: indexPath) as? ExampleListCell else { return UITableViewCell() }
+        
+        cell.titleLabel.text = "실험중"
+        
+        return cell
     }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 }
