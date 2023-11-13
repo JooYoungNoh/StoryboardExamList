6b355439b094d5420eed67d6397366a08aa32a48
NohJooYoung <nohjoo97@naver.com>
2023-11-13 16:31:39 +0900

    2023_11_13 실습 화면 이동

--- ExampleList/ExampleList/ExampleListVC.swift
+++ ExampleList/ExampleList/ExampleListVC.swift
@@ -11,7 +11,7 @@ class ExampleListVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    private var examList: [String] = ["InteractionExamVC", "AutoLayoutOneVC", "AutoLayoutTwoVC", "AutoLayoutThreeVC", "AutoLayoutFourVC"]
+    private var examList: [String] = ["InteractionExamVC", "AutoLayoutOneVC", "AutoLayoutTwoVC", "AutoLayoutThreeVC", "AutoLayoutFourVC", "AutoLayoutFiveVC", "AutoLayoutSixVC"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
