6279877edbd4ef8dab6afce27643c07aa5e8917c
NohJooYoung <nohjoo97@naver.com>
2023-11-13 18:17:05 +0900

    [add] 실습 예제 5, 6 화면 구현

--- ExampleList/ExampleList/2023_11_13/AutoLayoutFiveVC.swift
+++ ExampleList/ExampleList/2023_11_13/AutoLayoutFiveVC.swift
@@ -6,24 +6,137 @@
 //
 
 import UIKit
+import Combine
+import SnapKit
 
 class AutoLayoutFiveVC: UIViewController {
+    
+    var viewModel = HeightViewModel()
+    var disposalbleBag = Set<AnyCancellable>()
+    
+    let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    
+    let greenView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    let helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    let worldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "World"
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    let twoHelloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    let twoWorldLabel: UILabel = {
+        let label = UILabel()
+        label.text = "World"
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.viewModel.height = self.view.frame.height/3
+        uiCreate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print(size.height/3)
+        self.viewModel.height = size.height/3
+        setBinding()
     }
-    */
-
+    
+    func setBinding(){
+        print("setBinding")
+        self.viewModel.$height.sink{ (updatedList: CGFloat) in
+            self.redView.snp.removeConstraints()
+            self.redView.snp.makeConstraints { make in
+                make.top.leading.equalToSuperview()
+                make.trailing.equalTo(self.view.snp.centerX)
+                make.height.equalTo(self.viewModel.height)
+            }
+            
+            self.greenView.snp.removeConstraints()
+            self.greenView.snp.makeConstraints { make in
+                make.top.trailing.equalToSuperview()
+                make.leading.equalTo(self.view.snp.centerX)
+                make.height.equalTo(self.viewModel.height)
+            }
+        }.store(in: &disposalbleBag)
+    }
+    
+    func uiCreate() {
+        self.view.addSubview(self.redView)
+        self.redView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.trailing.equalTo(self.view.snp.centerX)
+            make.height.equalTo(self.view.frame.height/3)
+        }
+        
+        self.view.addSubview(self.greenView)
+        self.greenView.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+            make.leading.equalTo(self.view.snp.centerX)
+            make.height.equalTo(self.view.frame.height/3)
+        }
+        
+        self.view.addSubview(twoHelloLabel)
+        twoHelloLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.snp.top).offset(100)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(80)
+            make.height.equalTo(30)
+        }
+        
+        self.view.addSubview(twoWorldLabel)
+        twoWorldLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.twoHelloLabel.snp.top)
+            make.leading.equalTo(self.twoHelloLabel.snp.trailing).offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(30)
+        }
+        
+        self.view.addSubview(helloLabel)
+        helloLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(20)
+            make.width.equalTo(80)
+            make.height.equalTo(30)
+        }
+        
+        self.view.addSubview(worldLabel)
+        worldLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(helloLabel.snp.trailing).offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(30)
+        }
+    }
 }
+
+class HeightViewModel: ObservableObject {
+    @Published var height: CGFloat = 0
+}
+
--- ExampleList/ExampleList/2023_11_13/AutoLayoutSixVC.swift
+++ ExampleList/ExampleList/2023_11_13/AutoLayoutSixVC.swift
@@ -6,24 +6,65 @@
 //
 
 import UIKit
+import SnapKit
 
 class AutoLayoutSixVC: UIViewController {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "fallGlass")
+        imageView.layer.cornerRadius = 20
+        imageView.layer.borderWidth = 0
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let autoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "auto머시기"
+        label.font = .systemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let tapMeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Tap Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 20
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        uiCreate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func uiCreate() {
+        
+        self.view.addSubview(self.imageView)
+        self.imageView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+        
+        self.view.addSubview(self.autoLabel)
+        self.autoLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.imageView.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(40)
+        }
+        
+        self.view.addSubview(self.tapMeButton)
+        self.tapMeButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalToSuperview().offset(-50)
+            make.height.equalTo(50)
+        }
     }
-    */
-
 }
