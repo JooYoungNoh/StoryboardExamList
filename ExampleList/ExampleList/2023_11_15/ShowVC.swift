af592bcbe83410a277e12f1078db7ebcc165fd09
NohJooYoung <nohjoo97@naver.com>
2023-11-15 18:37:28 +0900

    [add] 버튼으로 탭바 이동

--- ExampleList/ExampleList/2023_11_15/ShowVC.swift
+++ ExampleList/ExampleList/2023_11_15/ShowVC.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class ShowVC: UIViewController {
+    
+    let selectBearButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Go Bear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 20
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +24,21 @@ class ShowVC: UIViewController {
         uiCreate()
     }
     
+    @objc func goBear(_ sender: UIButton){
+        guard let customTabBar = self.tabBarController as? CustomTabBar else { return }
+        
+        customTabBar.onClick(customTabBar.bearButton)
+    }
+    
     func uiCreate() {
-        self.view.backgroundColor = .systemIndigo
+        self.view.backgroundColor = .systemBackground
+        
+        self.selectBearButton.addTarget(self, action: #selector(goBear(_:)), for: .touchUpInside)
+        self.view.addSubview(self.selectBearButton)
+        self.selectBearButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
     }
 }
