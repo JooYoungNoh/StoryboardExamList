987afde5ffcdb0f40433dec41541ec54a1370e42
NohJooYoung <nohjoo97@naver.com>
2023-11-15 11:50:37 +0900

    [add] 이벤트 실습 화면 구현

--- ExampleList/ExampleList/2023_11_15/EventVC.swift
+++ ExampleList/ExampleList/2023_11_15/EventVC.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import SnapKit
 
 class EventVC: UIViewController {
-
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    private let imageArr: [String] = ["blueGlass", "cuteGlass", "fallGlass", "prettyGlass", "showGlass", "whiteGlass"]
+    private var index: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.myButton.layer.cornerRadius = 20
+        self.myImageView.image = UIImage(named: imageArr[0])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonClick(_ sender: UIButton) {
+        self.index = self.index == 5 ? 0 : self.index+1
+        
+        self.myImageView.image = UIImage(named: imageArr[index])
+        
+        self.myImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(200)
+        }
     }
-    */
 
+    @IBAction func switchToggle(_ sender: UISwitch) {
+        self.myImageView.snp.removeConstraints()
+        if sender.isOn {
+            self.myImageView.snp.makeConstraints { make in
+                make.width.height.equalTo(200)
+            }
+        } else {
+            self.myImageView.snp.makeConstraints { make in
+                make.width.height.equalTo(100)
+            }
+        }
+    }
 }
