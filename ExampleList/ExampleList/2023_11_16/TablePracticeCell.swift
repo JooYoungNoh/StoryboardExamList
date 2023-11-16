0d60e5e138950ed4031eb1ec199125e6c77114c3
NohJooYoung <nohjoo97@naver.com>
2023-11-16 10:17:39 +0900

    2023_11_16 테이블 뷰 실습 initial commit

---
+++ ExampleList/ExampleList/2023_11_16/TablePracticeCell.swift
@@ -0,0 +1,23 @@
+//
+//  TablePracticeCell.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/16.
+//
+
+import UIKit
+
+class TablePracticeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
