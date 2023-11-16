c72d8f4b0dcbbfcd02518e3bf0211f9a23893f71
NohJooYoung <nohjoo97@naver.com>
2023-11-16 11:49:54 +0900

    [add] 이미지 추가 및 셀 삭제

--- ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
+++ ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
@@ -11,12 +11,21 @@ class TablePracticeVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var items = ["Kim", "Lee", "Park"]
+    var items = [["Kim", "glassesGlass"], ["Lee", "dinosaurGlass"], ["Park", "friendGlass"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableView.separatorStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
 
@@ -24,10 +33,30 @@ extension TablePracticeVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TablePracticeCell", for: indexPath) as? TablePracticeCell else { return UITableViewCell() }
         
+        cell.nameLabel.text = self.items[indexPath.row][0]
+        cell.profileImageView.image = UIImage(named: self.items[indexPath.row][1])
+        cell.profileImageView.layer.cornerRadius = 20
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140    }
+}
+
+extension TablePracticeVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "삭제"
     }
 }
