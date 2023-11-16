ae7dc259a63d0a42e4c487077351875636266090
NohJooYoung <nohjoo97@naver.com>
2023-11-16 10:26:13 +0900

    2023_11_16 실습 화면 이동

--- ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
+++ ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
@@ -8,22 +8,26 @@
 import UIKit
 
 class TablePracticeVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items = ["Kim", "Lee", "Park"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TablePracticeVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TablePracticeCell", for: indexPath) as? TablePracticeCell else { return UITableViewCell() }
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
 }
