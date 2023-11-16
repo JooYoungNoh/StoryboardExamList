cdc5fa858ee2c3eeee9c4a29e3557d30cc19c18c
NohJooYoung <nohjoo97@naver.com>
2023-11-16 15:45:04 +0900

    [add] SearchBar 추가 및 기능 구현

--- ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
+++ ExampleList/ExampleList/2023_11_16/TablePracticeVC.swift
@@ -18,10 +18,21 @@ class TablePracticeVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var items: [TablePracticeModel] = []
+    
+    //서치바
+    var searching: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    var matchArr: [TablePracticeModel] = []
+    var searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.uiButtonCreate()
+        self.uiSearchBarCreate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,6 +55,32 @@ class TablePracticeVC: UIViewController {
     
 }
 
+//MARK: 서치바 메소드
+extension TablePracticeVC {
+    func uiSearchBarCreate() {
+        //서치바 UI
+        self.searchController.searchBar.delegate = self
+        self.searchController.searchResultsUpdater = self
+        self.searchController.searchBar.placeholder = "Search Name"
+        self.searchController.obscuresBackgroundDuringPresentation = false
+        self.searchController.definesPresentationContext = true
+        
+        self.navigationItem.searchController = self.searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+    }
+}
+
+extension TablePracticeVC: UISearchBarDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        self.matchArr = self.items.filter( { (list: TablePracticeModel) -> Bool in
+            return list.attractionNames.lowercased().contains(text.lowercased())
+        })
+        self.tableView.reloadData()
+    }
+}
+
+//MARK: 버튼 메소드
 extension TablePracticeVC {
     @objc func buttonClick(_ sender: UIBarButtonItem) {
         guard let addView = self.storyboard?.instantiateViewController(identifier: "TableAddVC") as? TableAddVC else { return }
@@ -61,20 +98,28 @@ extension TablePracticeVC {
     }
 }
 
+
+//MARK: 테이블 뷰 메소드
 extension TablePracticeVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TablePracticeCell", for: indexPath) as? TablePracticeCell else { return UITableViewCell() }
-        cell.selectionStyle = .none
         
-        cell.nameLabel.text = self.items[indexPath.row].attractionNames
-        cell.profileImageView.image = UIImage(named: self.items[indexPath.row].attractionImages)
+        cell.selectionStyle = .none
         cell.profileImageView.layer.cornerRadius = 20
+        
+        if self.searching {
+            cell.nameLabel.text = self.matchArr[indexPath.row].attractionNames
+            cell.profileImageView.image = UIImage(named: self.matchArr[indexPath.row].attractionImages)
+        } else {
+            cell.nameLabel.text = self.items[indexPath.row].attractionNames
+            cell.profileImageView.image = UIImage(named: self.items[indexPath.row].attractionImages)
+        }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
+        return self.searching ? self.matchArr.count : self.items.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -86,7 +131,7 @@ extension TablePracticeVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let nextView = self.storyboard?.instantiateViewController(identifier: "TableDetailVC") as? TableDetailVC else { return }
         
-        nextView.webSite = self.items[indexPath.row].webAddresses
+        nextView.webSite = self.searching ? self.matchArr[indexPath.row].webAddresses : self.items[indexPath.row].webAddresses
         
         self.navigationController?.pushViewController(nextView, animated: true)
     }
