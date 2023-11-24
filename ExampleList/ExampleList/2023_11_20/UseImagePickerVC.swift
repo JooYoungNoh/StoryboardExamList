d0c9de756c16d6da8afd23ced931a64a9fde102c
NohJooYoung <nohjoo97@naver.com>
2023-11-24 14:24:10 +0900

    [add] 이미지 피커 카메라 사용하기 실습

--- ExampleList/ExampleList/2023_11_20/UseImagePickerVC.swift
+++ ExampleList/ExampleList/2023_11_20/UseImagePickerVC.swift
@@ -6,13 +6,43 @@
 //
 
 import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
 
 class UseImagePickerVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var newMeida: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+}
+
+extension UseImagePickerVC {
+    @IBAction func useCamera(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = [UTType.image.identifier]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true)
+            
+            self.newMeida = true
+        }
+    }
+    
+    @IBAction func useCameraRoll(_ sender: UIBarButtonItem) {
+        
+    }
+}
+
+extension UseImagePickerVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 }
