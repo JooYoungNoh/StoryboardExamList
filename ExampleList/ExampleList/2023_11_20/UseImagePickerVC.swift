e0050eedb752bcafdab118f6506b5da80764706d
NohJooYoung <nohjoo97@naver.com>
2023-11-24 14:01:58 +0900

    2023_11_24 실습 initial commit 및 화면 이동

---
+++ ExampleList/ExampleList/2023_11_20/UseImagePickerVC.swift
@@ -0,0 +1,18 @@
+//
+//  UseImagePickerVC.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/24.
+//
+
+import UIKit
+
+class UseImagePickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
