7fcd1b4e146334e0ab1c39356f258534201b58d9
NohJooYoung <nohjoo97@naver.com>
2023-12-04 11:23:22 +0900

    2023_12_04 실습 first commit 및 화면 이동

---
+++ ExampleList/ExampleList/2023_12_04/LiveSpeachVC.swift
@@ -0,0 +1,18 @@
+//
+//  LiveSpeachVC.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/12/04.
+//
+
+import UIKit
+
+class LiveSpeachVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
